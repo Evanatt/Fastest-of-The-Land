54cd9d6caffdad5ac883c07384fee3a96fd80f90
Emanuel Apaza <emanuel.apaza.200325@gmail.com>
2024-11-08 05:29:47 -0300

    Número de versión en el menu

--- Fastest of The Land/Assets/scripts/botones_main.cs
+++ Fastest of The Land/Assets/scripts/botones_main.cs
@@ -1,11 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class botones_main : MonoBehaviour
 {
+    public TMP_Text version;
 
+    void Start()
+    {
+        version.text = Application.version;
+    }
     public void jugar()
     {
         SceneManager.LoadScene("nivel_garden_race");
